ae147afb8060551a9ff3c3d7aadae0df69f27897
alaa-hossam <alaa.hossam.2013@gmail.com>
2023-11-11 10:24:22 +0200

    edit account class

--- IstaPay/Account.java
+++ IstaPay/Account.java
@@ -1,4 +1,35 @@
 package IstaPay;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.Scanner;
 
-public class Account {
+abstract public class Account {
+    protected String accountNum;
+
+    public boolean verifyMobileNum(String phone) {
+        Pattern pat = Pattern.compile("^\\d{11}$");
+        Matcher mat = pat.matcher(phone);
+        Scanner obj = new Scanner(System.in);
+        String OTP = "171932", OTPverify;
+        if (mat.find()) {
+            System.out.println("Your OTP is: " + OTP);
+            OTPverify = obj.nextLine();
+            if (OTP == OTPverify) {
+                return true;
+            }else {
+                System.out.println("Unvalid OTP.PLease try again later");
+                return false;
+            }
+        }
+        return false;
+    }
+    abstract public boolean verifyAccount(String phone);
+
+    public void setAccountNum(String accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public String getAccountNum() {
+        return accountNum;
+    }
 }
