08c538dfd8ce83a2122709b12765495c40f6c657
Asmaa saleh <92892342+AsmaaSal@users.noreply.github.com>
2023-11-14 15:22:34 +0200

    Update ToBankAcc.java

--- IstaPay/ToBankAcc.java
+++ IstaPay/ToBankAcc.java
@@ -3,21 +3,20 @@ import java.util.Map;
 public class ToBankAcc implements Transfer{
    // private Customer user;
     @Override
-    public void ExecuteTransfer(String accountNum, double amount,Customer user){
+    public void ExecuteTransfer(String username, double amount,Customer user){
 
         if(user.getAcc() instanceof BankAcc) {
             for (Customer found : user.customers) {
-                //String a=user.getAcc().getAccountNum();
-                if (accountNum.equals(found.getAcc().getAccountNum())) {
+                if (username.equals(found.getUserName())) {
                     Customer receiver = found;
                     receiver.deposit(amount);
                     user.withdraw(amount);
+
 
                 }
             }
         }else {
             System.out.println("you must transfer from BANK ACCOUNT \n");
         }
-
     }
 }
